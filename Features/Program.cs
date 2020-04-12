1e1d8d184e6d6d72cc84a66297660eafb0c06e1c
Ivan Vasquez <ivan.vasquez@niceincontact.com>
2020-04-12 18:39:20 -0400

    Add project files.

---
+++ Features/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> square = x => x * x;
+            //Func<int, int, int> add = (x, y) => x + y;
+            Func<int, int, int> add = (x, y) =>
+            {
+                int temp = x + y;
+                return temp;
+            };
+
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(square(add(3, 5)));
+
+
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott" },
+                new Employee { Id = 2, Name = "Chris" }
+            };
+
+            var sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            //Console.WriteLine(sales.Count());
+
+            //IEnumerator<Employee> enumerator = sales.GetEnumerator();
+
+            //while (enumerator.MoveNext())
+            //{
+            //    Console.WriteLine(enumerator.Current.Name);
+            //}
+
+            //foreach (var employee in developers.Where(
+            //    delegate (Employee employee)
+            //    {
+            //        return employee.Name.StartsWith("S");
+            //    }))
+            //{
+            //    Console.WriteLine(employee.Name);
+            //}
+
+
+            // Method syntax
+            var query = developers.Where(e => e.Name.Length == 5)
+                                  .OrderBy(e => e.Name)
+                                  .Select(e => e);  // esto es completamente opcional para la notacion de metodo
+
+            // Query syntax
+            var query2 = from developer in developers
+                         where developer.Name.Length == 5
+                         orderby developer.Name
+                         select developer;
+
+
+            foreach (var employee in query2)
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+
+
+        private static bool NameStartsWithS(Employee employee)
+        {
+            return employee.Name.StartsWith("S");
+        }
+    }
+}
---
+++ LinqSamples/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+
+            Console.WriteLine("******");
+
+            ShowLargeFilesWithLinq(path);
+
+            Employee[] developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott" },
+                new Employee { Id = 2, Name = "Chris" }
+            };
+
+            List<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            foreach (var person in sales)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            //var query = from file in new DirectoryInfo(path).GetFiles()
+            //            orderby file.Length descending
+            //            select file;
+
+            //foreach (var file in query.Take(5))
+            //{
+            //    Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            //}
+
+            var query = new DirectoryInfo(path).GetFiles()
+                .OrderByDescending(f => f.Length)
+                .Take(5);
+
+            foreach (var file in query)
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+            
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            //foreach (FileInfo file in files)
+            //{
+            //    Console.WriteLine($"{file.Name} : {file.Length}");
+            //}
+
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
