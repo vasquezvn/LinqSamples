1afb32efc40b896110e97dddde6c9f7ab32f9401
Ivan Vasquez <ivan.vasquez@niceincontact.com>
2020-04-24 22:07:15 -0400

    Update program

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -13,14 +13,33 @@ namespace Cars
         {
             var cars = ProcessFile("fuel.csv");
 
-            foreach (var car in cars)
+            // METHOD SYNTAX
+            //var query = cars.OrderByDescending(c => c.Combined)
+            //                .ThenBy(c => c.Name);   // permite agregar una segunda manera de ordernar 
+
+            // QUERY SYNTAX
+            var query =
+                from car in cars
+                where car.Manufacturer == "BMW" && car.Year == 2016     // asi se filtra por mas de un criterio
+                orderby car.Combined descending, car.Name ascending  // esta es la manera que aplicas mas de una manera de ordenar
+                select car;
+
+            var query2 =
+                cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                .OrderByDescending(c => c.Combined)
+                .ThenBy(c => c.Name)
+                .Select(c => c);
+
+
+            foreach (var car in query2.Take(10))
             {
-                Console.WriteLine(car.Name);
+                Console.WriteLine($"{car.Manufacturer} {car.Name} : {car.Combined}");
             }
         }
 
         private static List<Car> ProcessFile(string path)
         {
+            // METHOD SYNTAX
             //return
             //File.ReadAllLines(path)
             //    .Skip(1)
@@ -28,6 +47,8 @@ namespace Cars
             //    .Select(Car.ParseFromCsv)
             //    .ToList();
 
+
+            // QUERY SYNTAX
             var query =
 
                 from line in File.ReadAllLines(path).Skip(1)
